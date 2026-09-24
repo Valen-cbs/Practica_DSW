6452be36161564f4f0b1096b61786cf298e68305
Valentino <valen.cabas@gmail.com>
2026-09-24 16:56:31 -0300

    add: api http

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-const mensaje: string = "TypeScript funciona correctamente";
+import express, { Request, Response } from "express";
 
-console.log(mensaje);
+const app = express();
+const PORT = 3000;
+
+// Middleware para poder recibir JSON
+app.use(express.json());
+
+// Ruta GET
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    mensaje: "API funcionando correctamente"
+  });
+});
+
+// Ruta POST
+app.post("/saludar", (req: Request, res: Response) => {
+  const { nombre } = req.body;
+
+  if (!nombre) {
+    res.status(400).json({
+      error: "Debe enviar un nombre"
+    });
+    return;
+  }
+
+  res.status(200).json({
+    mensaje: `Hola ${nombre}`
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
